45f24094b063aefb31d1c0da976ed3395b07a495
siyonapande2025ainds-svg <siyonapande2025.ainds@mmcoe.edu.in>
2026-09-16 14:12:23 +0530

    Integrate Navbar, MapView, and Sidebar into App
    Added Navbar, MapView, and Sidebar components to the App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import Navbar from './components/Navbar';
+import MapView from './components/MapView';
+import Sidebar from './components/Sidebar';
 
+export default function App() {
+  return (
+    <div className="flex flex-col h-screen w-screen overflow-hidden bg-slate-900 text-white">
+      <Navbar />
+      <div className="flex flex-1 relative overflow-hidden">
+        <Sidebar />
+        <MapView />
+      </div>
+    </div>
+  );
+}
